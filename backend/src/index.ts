e17d4a38cd7b5f4a4d35749db72df83749771c77
Pavank Khaire <pavankh04@gmail.com>
2025-05-28 13:10:33 +0530

    implements admin dashboard stats routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -58,6 +58,57 @@ app.post('/app/admin/add-store', authMiddleware, async (req, res) => {
 
 })
 
+app.get('/app/admin/dashboard/stats', authMiddleware, async (req, res) => {
+
+    //@ts-ignore
+    if (req.user.role !== 'admin') {
+        res.json({
+            error: "Access Denied"
+        })
+    }
+
+    const totalUsers = await pgClient.query(`SELECT COUNT(*) FROM users`)
+    const totalStores = await pgClient.query(`SELECT COUNT(*) FROM stores`)
+    const totalRatings = await pgClient.query(`SELECT COUNT(*) FROM ratings`)
+
+    res.json({
+        totalUsers: Number(totalUsers.rows[0].count),
+        totalStores: Number(totalStores.rows[0].count),
+        totalRatings: Number(totalRatings.rows[0].count),
+    })
+})
+
+app.get('/app/admin/dashboard/stores', authMiddleware, async (req, res) => {
+
+    //@ts-ignore
+    if (req.user.role !== 'admin') {
+        res.json({
+            error: "Access Denied"
+        })
+    }
+
+    const storesDetails = await pgClient.query(`SELECT s.name, s.email, s.address, ROUND(AVG(r.rating), 2) AS rating FROM stores s LEFT JOIN ratings r ON s.id = r.store_id GROUP BY s.id`);
+    res.json({
+        storesDetails: storesDetails.rows
+    })
+})
+
+app.get('/app/admin/dashboard/users', authMiddleware, async (req, res) => {
+
+    //@ts-ignore
+    if (req.user.role !== 'admin') {
+        res.json({
+            error: "Access Denied"
+        })
+    }
+
+    const usersDetails = await pgClient.query(`SELECT u.name, u.email, u.address, u.role, ROUND(AVG(r.rating), 2) AS owner_rating
+                                               FROM  users u LEFT JOIN  stores s ON u.id = s.owner_id LEFT JOIN ratings r ON s.id = r.store_id 
+                                               GROUP BY u.id, u.name, u.email, u.address, u.role;`);
+    res.json({
+        usersDetails: usersDetails.rows
+    })
+})
 
 app.post('/app/user/signup', async (req, res) => {
 
