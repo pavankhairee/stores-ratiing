2e8e538eb4cb93ed20dc9b73a67a87acf564d982
Pavank Khaire <pavankh04@gmail.com>
2025-05-29 11:37:27 +0530

    update rating button add condition to it

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -20,40 +20,50 @@ export function Home() {
                     Authorization: localStorage.getItem("token")
                 }
             });
-            setStore(response.data.storesList)
-
+            setStore(response.data.storesList);
         }
-        getDetails();
+        getDetails()
     }, [])
 
     const handleAddRating = (store_id: number) => {
 
-        const rating = prompt('Enter your rating (1-5)')
-        if (rating) {
-            axios.post("http://localhost:3000/app/user/rating", {
-                store_id: store_id,
-                rating: parseFloat(rating)
-            }, {
-                headers: {
-                    Authorization: localStorage.getItem("token") || ""
-                }
-            }).then(() => window.location.reload());
+        const rating = prompt('Enter New Rating (1-5)')
+        const parseRating = parseFloat(rating || "")
+        if (!rating || isNaN(parseRating) || parseRating < 1 || parseRating > 5) {
+            alert('Enter a Valid Rating between 1 and 5');
+            return;
         }
+
+        axios.post("http://localhost:3000/app/user/rating", {
+            store_id: store_id,
+            rating: parseFloat(rating)
+        }, {
+            headers: {
+                Authorization: localStorage.getItem("token") || ""
+            }
+        }).then(() => window.location.reload());
+
+
     }
 
     const handleUpdateRating = (store_id: number) => {
 
         const rating = prompt('Enter New Rating (1-5)')
-        if (rating) {
-            axios.post("http://localhost:3000/app/user/rating", {
-                store_id: store_id,
-                rating: parseFloat(rating)
-            }, {
-                headers: {
-                    Authorization: localStorage.getItem("token") || ""
-                }
-            }).then(() => window.location.reload());
+        const parseRating = parseFloat(rating || "")
+        if (!rating || isNaN(parseRating) || parseRating < 1 || parseRating > 5) {
+            alert('Enter a Valid Rating between 1 and 5');
+            return;
         }
+
+        axios.post("http://localhost:3000/app/user/rating", {
+            store_id: store_id,
+            rating: parseFloat(rating)
+        }, {
+            headers: {
+                Authorization: localStorage.getItem("token") || ""
+            }
+        }).then(() => window.location.reload());
+
     }
 
     return (
@@ -66,18 +76,18 @@ export function Home() {
                         <p className="text-gray-800">Your Rating: {s.user_rating ?? 'Not Rated'}</p>
                         <p className="text-gray-800">Avg. Rating: {s.overall_rating ?? 'No ratings yet'}</p>
                         <div className="mt-4 space-x-2">
-                            <Button
+                            {!s.user_rating && <Button
                                 className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600"
                                 onClick={() => handleAddRating(s.id)}
                             >
                                 Add Rating
-                            </Button>
-                            <Button
+                            </Button>}
+                            {s.user_rating && <Button
                                 className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600"
                                 onClick={() => handleUpdateRating(s.id)}
                             >
                                 Update Rating
-                            </Button>
+                            </Button>}
                         </div>
                     </div>
                 ))}
